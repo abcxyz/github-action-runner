2759e8dfb19667f9ad72ce255658587d527f1b45
Dylan Yurgionas <dcreey@google.com>
2025-12-05 16:11:32 +0000

    chore: dump pre-run.sh logs

--- runner/ubuntu-24-04/pre-run.sh
+++ runner/ubuntu-24-04/pre-run.sh
@@ -1,9 +1,28 @@
 #!/bin/bash
 set -euo pipefail
 
+# log to a file to that gets dumped to stdout in cloud build logs.
+PRERUN_LOG_FILE="/tmp/pre-run.log"
+function log {
+  if [ ! -f "${PRERUN_LOG_FILE}" ]; then
+    touch "${PRERUN_LOG_FILE}"
+  fi
+  "$@" 2>&1 | tee -a "$PRERUN_LOG_FILE"
+    
+  local status="${PIPESTATUS[0]}"
+  if [ "${status}" -ne 0 ]; then
+    echo "Command \"$*\" failed with status ${status}. Check ${PRERUN_LOG_FILE} for details." | tee -a "${PRERUN_LOG_FILE}"
+  fi
+  return "${status}"
+}
+
 LOCK_FILE="/tmp/runner.lock"
 touch "${LOCK_FILE}"
-echo "Runner lock file created at ${LOCK_FILE}. Idle timeout is now disabled."
+log echo "Runner lock file created at ${LOCK_FILE}. Idle timeout is now disabled."
 
-echo "pre-run.sh script"
-printenv
+log echo "pre-run.sh script"
+log printenv
+
+GOOGLE_TOKEN="this-is-a-test"
+echo "GOOGLE_TOKEN=${GOOGLE_TOKEN}" >> /actions-runner/.env
+export GOOGLE_TOKEN="${GOOGLE_TOKEN}"
