2b83690a4a468f30174e51aebb0d64543c2849d3
Dylan Yurgionas <dcreey@google.com>
2025-12-05 18:25:14 +0000

    fix: lint

--- runner/ubuntu-24-04/pre-run.sh
+++ runner/ubuntu-24-04/pre-run.sh
@@ -4,16 +4,13 @@ set -euo pipefail
 # log to a file to that gets dumped to stdout in cloud build logs.
 PRERUN_LOG_FILE="/tmp/pre-run.log"
 touch "${PRERUN_LOG_FILE}"
-function log {
-  echo "$@" >> "${PRERUN_LOG_FILE}"
-}
 
 LOCK_FILE="/tmp/runner.lock"
 touch "${LOCK_FILE}"
-echo "Runner lock file created at ${LOCK_FILE}. Idle timeout is now disabled." | log
+echo "Runner lock file created at ${LOCK_FILE}. Idle timeout is now disabled." >> "${PRERUN_LOG_FILE}"
 
-echo "pre-run.sh script" | log
-printenv | log
+echo "pre-run.sh script" >> "${PRERUN_LOG_FILE}"
+printenv >> "${PRERUN_LOG_FILE}"
 
 GOOGLE_TOKEN="this-is-a-test"
 echo "GOOGLE_TOKEN=${GOOGLE_TOKEN}" >> /actions-runner/.env
