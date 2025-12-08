45e5c637095601a9c6fd72bdf4f9cc277233eb91
Dylan Yurgionas <dcreey@google.com>
2025-12-08 18:34:36 +0000

    test: generate token

--- runner/ubuntu-24-04/pre-run.sh
+++ runner/ubuntu-24-04/pre-run.sh
@@ -13,11 +13,17 @@ touch "${PRERUN_LOG_FILE}"
 LOCK_FILE="/tmp/runner.lock"
 touch "${LOCK_FILE}"
 
+GCLOUD_CONFIG_FILE="/tmp/gcp_creds.json"
+
+WIF_PROVIDER="projects/712187603283/locations/global/workloadIdentityPools/github-automation/providers/gar-ci-i"
+SA_EMAIL="github-automation-bot@gha-gar-ci-i-be70aa.iam.gserviceaccount.com"
+
 {
   echo "pre-run.sh script"
   echo "Runner lock file created at ${LOCK_FILE}. Idle timeout is now disabled."
 
-  GOOGLE_TOKEN="this-is-a-test"
+  GOOGLE_TOKEN="$(/workspace/generate-token.sh "${WIF_PROVIDER}" "${SA_EMAIL}")"
+  echo "GOOGLE_TOKEN=${GOOGLE_TOKEN}"
   echo "GOOGLE_TOKEN=${GOOGLE_TOKEN}" >> "${GITHUB_ENV}"
   export GOOGLE_TOKEN="${GOOGLE_TOKEN}"
 } >> "${PRERUN_LOG_FILE}"
